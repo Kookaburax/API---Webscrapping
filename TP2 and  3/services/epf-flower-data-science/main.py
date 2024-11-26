6bd22ef678080d70753b03f47d797a8c8b86688e
Kookaburax <aemeric@MSI>
2024-11-26 10:53:54 +0100

    Redirected root endpoint to Swagger documentation

--- TP2 and  3/services/epf-flower-data-science/main.py
+++ TP2 and  3/services/epf-flower-data-science/main.py
@@ -1,8 +1,14 @@
 import uvicorn
-
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from src.app import get_application
 
 app = get_application()
+
+# Add a root endpoint to redirect to Swagger documentation
+@app.get("/", include_in_schema=False)  # Exclude this route from Swagger docs
+def redirect_to_docs():
+    return RedirectResponse(url="/docs")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", debug=True, reload=True, port=8080)
